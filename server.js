8645904263044d72564714446eba911c1fca984d
AniaJezierska <candyrockstore5@gmail.com>
2021-01-11 16:00:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,52 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+let port = 8800;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+// Get Route
+app.get('/all', getProjectData);
+
+function getProjectData(request, response) {
+    response.send(projectData);
+    console.log(projectData);
+};
+
+// Post Route
+app.post('/add', postProjectData);
+
+function postProjectData(request, response) {
+    newEntry = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        feelings: request.body.feelings,
+    }
+    response.send(newEntry);
+    console.log(newEntry);
+};
